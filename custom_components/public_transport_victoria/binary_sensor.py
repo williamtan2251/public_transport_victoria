e7527c2def18a7ab4dbd7c4aeae1842c64cab13c
William Tan <20216106+williamtan2251@users.noreply.github.com>
2025-11-07 23:15:42 +1100

    Fix identifiers and name attributes in binary sensor

--- custom_components/public_transport_victoria/binary_sensor.py
+++ custom_components/public_transport_victoria/binary_sensor.py
@@ -36,8 +36,8 @@ class PTVCurrentDisruptionsBinarySensor(CoordinatorEntity, BinarySensorEntity):
         self._attr_unique_id = f"{connector.route}-{connector.direction}-{connector.stop}-disruptions"
         self._attr_icon = "mdi:alert"
         self._attr_device_info = {
-            "identifiers": {(DOMAIN, f"{connector.route}-{connector.direction}-{connector.stop}")},
-            "name": f"{self.coordinator.connector.route_name} line",
+            "identifiers": {(DOMAIN, str(connector.route))},
+            "name": f"{connector.route_name} line",
             "manufacturer": "Public Transport Victoria",
             "model": f"{connector.stop_name} to {connector.direction_name}",
         }
